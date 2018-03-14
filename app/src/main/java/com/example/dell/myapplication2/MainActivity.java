ba46f21d49c3afa85ed005ebf77879a2e7998a24
DESKTOP-B8G6NFV\user <purva.vansia@yahoo.com>
2018-03-14 15:49:19 -0500

    updated

--- app/src/main/java/com/example/dell/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication2/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void TestGit(){
         int i=1+1;
-        String name="abdul";
+        //update by purva
+        String name="purva";
     }
 }
